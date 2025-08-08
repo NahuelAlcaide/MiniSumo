c810901d64da088a6060a18b8aa6489c24a46d54
Nahuel Alcaide <nahuel.alcaide@metaok.com>
2025-08-08 20:15:38 -0300

    Valores para pulsos mas fuertes

--- include/config.h
+++ include/config.h
@@ -28,9 +28,10 @@
 #define LEFT_LED_PIN 8
 
 // Parámetros sensores frontales
+// 1000 micros con 0.01 de DC para mas potencia
 #define IR_LED_COUNT 3
-#define IR_MICROS_DELAY 1000
-#define IR_DUTY_CYCLE 0.01 // Porcentaje de tiempo que el led está prendido
+#define IR_MICROS_DELAY 100
+#define IR_DUTY_CYCLE 0.05 // Porcentaje de tiempo que el led está prendido
 
 // PARAMETROS DE BATALLA
 #define SEEK_THRESHOLD 60
